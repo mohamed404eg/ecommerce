ccca4bf6490ce4319dc38537f14b741f95387868
mohamed404eg <mohamed404eg@gmail.com>
2023-03-30 01:19:16 +0200

    updata

--- Shop-cart.js
+++ Shop-cart.js
@@ -1,178 +1,148 @@
+    //* thead start
+    let TtheadPordect = document.querySelector("#TtheadPordect");
 
-//* thead start
-let TtheadPordect = document.querySelector("#TtheadPordect");
+    // creat th in table
+    let thAll = `<tr>
+    <th>Products</th>
+    <th>Color & Size</th>
+    <th>Quantity</th>
+    <th>Price</th>
+    <th>Total</th>
+    </tr>
 
-// creat th in table
-let thAll = `<tr>
-<th>Products</th>
-<th>Color & Size</th>
-<th>Quantity</th>
-<th>Price</th>
-<th>Total</th>
-</tr>
+    `;
 
-`;
+    // creat th in table
+    TtheadPordect.innerHTML += thAll;
 
-// creat th in table
-TtheadPordect.innerHTML += thAll;
+    // thead end
 
-// thead end
-
-
-// Loop LocalStorage IN Prodect 
-function LoopLocalStorage() {
-
-
-
-for (let i = 0; i < localStorage.length; i++) {
-    // import localStorage prodect
-    let dataProdect = JSON.parse(localStorage.getItem(localStorage.key(i)));
-   
-
-    let tr = document.createElement("tr");
+    // Loop LocalStorage IN Prodect
+    function LoopLocalStorage() {
+        for (let i = 0; i < localStorage.length; i++) {
+        
+        // import localStorage prodect
     
-
-    // td
-    let td1 = document.createElement("td");
-    let td2 = document.createElement("td");
-    let td3 = document.createElement("td");
-    let td4 = document.createElement("td");
-    let td5 = document.createElement("td");
-  
-
-    td1.innerHTML = `<img class="imgProdect" src="${dataProdect.img}" alt=""> <p class="Prodecttitle"> ${ dataProdect.title}</p>`;
-    td2.innerHTML = `<p class="Color-Size">none</p>`;
-    td3.innerHTML = `<input class="inputOrders" index="${dataProdect.id}" type="number" value="${dataProdect.orders}"> ` ;
-    td4.innerHTML = `$${dataProdect.price}`;
-
  
-    
-    
-    // append to tr 
-    tr.append(td1)
-    tr.append(td2)
-    tr.append(td3)
-    tr.append(td4)
-    tr.append(td5)
-    
-    
-    // tbody 
-    let TbodyPordect = document.getElementById("TbodyPordect")
+        let dataProdect = JSON.parse(localStorage.getItem(localStorage.key(i)));
 
-    // append tr to tbody
-        TbodyPordect.append(tr);
-   
-    
-    let InputOrders = document.querySelectorAll(".inputOrders")
+      
+        let tr = document.createElement("tr");
 
+        // td
+        let td1 = document.createElement("td");
+        let td2 = document.createElement("td");
+        let td3 = document.createElement("td");
+        let td4 = document.createElement("td");
+        let td5 = document.createElement("td");
 
+        td1.innerHTML = `<img class="imgProdect" src="${dataProdect.img}" alt=""> <p class="Prodecttitle"> ${dataProdect.title}</p>`;
+        td2.innerHTML = `<p   class="Color-Size">none</p>`;
+        td3.innerHTML = `<span class="Quantity">Quantity</span><input class="inputOrders" index="${dataProdect.id}" type="number" value="${dataProdect.orders}"> `;
+        td4.innerHTML = `$${dataProdect.price}`;
 
- // Total price
-    function addAndUpdata() {
-
-        td5.innerHTML = `<p class="p-bottom">$${(dataProdect.price * InputOrders[i].value).toFixed(2), dataProdect.price * InputOrders[i].value} <button class="delet" onclick="delet('${dataProdect.id}')"><i class="fa-sharp fa-solid fa-circle-xmark"></i> </button></p>`;
-    }
-    // call this function in updata totele price
-    addAndUpdata() 
-    
-
-
-    // *UPdata Quantity
-    InputOrders[i].addEventListener("keyup", () => {
+        // set attr table moblie max-width: 954px
+        td2.setAttribute("data-label", "Color & Size");
+        td4.setAttribute("data-label", "price");
+        td5.setAttribute("data-label", "Total");
         
 
 
-        let indexN = InputOrders[i].getAttribute('index');
+        // append to tr
+        tr.append(td1);
+        tr.append(td2);
+        tr.append(td3);
+        tr.append(td4);
+        tr.append(td5);
 
+        // tbody
+        let TbodyPordect = document.getElementById("TbodyPordect");
+
+        // append tr to tbody
+        TbodyPordect.append(tr);
+
+        let InputOrders = document.querySelectorAll(".inputOrders");
+
+        // Total price
+        function addAndUpdata() {
+        td5.innerHTML = `<p data-label="Amount" class="p-bottom">$${
+            ((dataProdect.price * InputOrders[i].value).toFixed(2),
+            dataProdect.price * InputOrders[i].value)
+        } <button class="delet" onclick="delet('${
+            dataProdect.id
+        }')"><i class="fa-sharp fa-solid fa-circle-xmark"></i> </button></p>`;
+        }
+        // call this function in updata totele price
+        addAndUpdata();
+
+        // *UPdata Quantity
+        InputOrders[i].addEventListener("keyup", () => {
+        let indexN = InputOrders[i].getAttribute("index");
 
         for (let i = 0; i < localStorage.length; i++) {
             // import localStorage prodect
             let dataProdect = JSON.parse(localStorage.getItem(localStorage.key(i)));
-    
+
             if (dataProdect.id == indexN) {
-               
-                // get value new
-                let QuantityNEW = InputOrders[i].value;
+            // get value new
+            let QuantityNEW = InputOrders[i].value;
 
-                dataProdect.orders = QuantityNEW;
-                console.log(dataProdect.orders)
-                
+            dataProdect.orders = QuantityNEW;
+            console.log(dataProdect.orders);
 
-                // to uplod localStorage new QuantityNEW
-                let ToStraing = JSON.stringify(dataProdect);
-                localStorage.setItem(`${dataProdect.id}`, ToStraing)
-                
-                 // call this function in updata totele price
-                 addAndUpdata() 
-              
+            // to uplod localStorage new QuantityNEW
+            let ToStraing = JSON.stringify(dataProdect);
+            localStorage.setItem(`${dataProdect.id}`, ToStraing);
+
+            // call this function in updata totele price
+            addAndUpdata();
             }
-    
-
-    
         }
+        });
 
-
-
-
-
-
-    }
-    
-    
-    );
-
-
-
-  
-    // add btn to bottom
-    if (i +1 >= localStorage.length ) {
+        // add btn to bottom
+        if (i + 1 >= localStorage.length) {
         let tdend = document.createElement("td");
         let trend = document.createElement("tr");
         trend.className = "trend";
         tdend.append(trend);
         trend.innerHTML = `
-        <td class="tdend-TEMP"></td>
-        <td class="tdend-TEMP"></td>
-        <td class="tdend-TEMP"></td>
-        <td class="tdend-TEMP">  <bottom class="UpdateCart">Update Cart</bottom></td>
+            <td class="tdend-TEMP"></td>
+            <td class="tdend-TEMP"></td>
+            <td class="tdend-TEMP"></td>
+            <td class="tdend-TEMP">  <bottom  onclick="location.reload" class="UpdateCart">Update Cart</bottom></td>
+        
+            <div class="btn-table-b">
+    
+    <bottom onclick="location.href = 'https://mohamed404eg.github.io/ecommerce'" class="ContinueShopping">Continue shopping</bottom>
+    </div>`;
+        TbodyPordect.append(trend);
+        }
+        }
        
-        <div class="btn-table-b">
- 
-   <bottom class="ContinueShopping">Continue shopping</bottom>
-</div>`;
-TbodyPordect.append(trend)
-
-  }
     
+        // LoopLocalStorage() end her
+    }
 
+    window.addEventListener("DOMContentLoaded", LoopLocalStorage);
 
-}
-}
-
-window.addEventListener("DOMContentLoaded",LoopLocalStorage)
-    
-
-// *delete prodect
-function delet(index){
+    // *delete prodect
+    function delet(index) {
     for (let i = 0; i < localStorage.length; i++) {
         // import localStorage prodect
         let dataProdect = JSON.parse(localStorage.getItem(localStorage.key(i)));
 
         if (dataProdect.id == index) {
-            localStorage.removeItem(index);
+        localStorage.removeItem(index);
 
-            // Loop LocalStorage prodect after delet
-            
-            let TbodyPordect = document.getElementById("TbodyPordect")
-            TbodyPordect.innerText = " ";
-            LoopLocalStorage();
+        // Loop LocalStorage prodect after delet
+
+        let TbodyPordect = document.getElementById("TbodyPordect");
+        TbodyPordect.innerText = " ";
+        LoopLocalStorage();
         }
+        }
+        
 
-
-    }
-    
-}
-    
-
-
-
+      
+    } 
