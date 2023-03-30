111a55ed8a18b170696870303fe61ec021301edb
mohamed404eg <mohamed404eg@gmail.com>
2023-03-30 23:22:39 +0200

    updata

---
+++ menu.js
@@ -0,0 +1,13 @@
+let menu = document.querySelector(".menu");
+let listMenu = document.querySelector(".listMenu");
+menu.addEventListener("click", () => {
+    
+    listMenu.classList.toggle("listMenuToggle")
+})
+
+let menuclose = document.querySelector(".menuclose");
+menuclose.addEventListener("click", () => {
+    listMenu.classList.toggle("listMenuToggle")
+})
+
+console.log(menuclose)
