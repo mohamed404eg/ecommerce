ad31a7126d39b5f2339d6568d34ae6dfbce70449
mohamed404eg <mohamed404eg@gmail.com>
2023-04-04 04:02:50 +0200

    updata

--- main.js
+++ main.js
@@ -405,14 +405,14 @@ function getRatingsFeatured() {
 
 function cartPlus() {
   let addToCartICON = document.querySelectorAll(".addToCart");
-  let plus = 0;
   console.log("i")
   for (let i = 0; i < addToCartICON.length; i++) {
-
+    let plus = 0;
     addToCartICON[i].addEventListener("click", ()=> {
       addToCartICON[i].innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="18.8" height="20" fill="currentColor" class="bi bi-cart-check-fill" viewBox="0 0 16 16">
       <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm-1.646-7.646-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L8 8.293l2.646-2.647a.5.5 0 0 1 .708.708z"/>
   </svg>`;
+      
       plus++;
 
       addToCartICON[i].innerHTML += plus;
@@ -428,8 +428,9 @@ function cartPlus() {
 cartPlus();
 function cartPlus2() {
   let addToCartICON = document.querySelectorAll(".FeaturedProductsaddToCart");
-  let plus = 0;
+
   for (let i = 0; i < addToCartICON.length; i++) {
+    let plus = 0;
     addToCartICON[i].addEventListener("click", () => {
       addToCartICON[i].innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="18.8" height="20" fill="currentColor" class="bi bi-cart-check-fill" viewBox="0 0 16 16">
       <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm-1.646-7.646-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L8 8.293l2.646-2.647a.5.5 0 0 1 .708.708z"/></svg>`;
